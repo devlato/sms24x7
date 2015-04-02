3cdb2627a29e1de84670acb858f90930f35978c0
devlato <github@devlato.com>
2015-04-02 05:45:39 +0300

    + sms24x7-client now has help and options

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@
 
 
 
-var SmsSender, smsSender, Minimist, cmdArgs; 
+var SmsSender, smsSender, Minimist, cmdArgs, command, printHelp; 
 
 
 
@@ -18,7 +18,39 @@ Minimist  = require('minimist');
 
 
 
-cmdArgs = Minimist(process.argv.slice(2));
+cmdArgs   = Minimist(process.argv.slice(2));
+command   = cmdArgs._[0]
+
+
+printHelp = function(showUnsupportedCommand) {
+	var lines = [];
+
+	if (showUnsupportedCommand) {
+		lines = lines.concat([
+			'',
+			'    Unsupported command',
+		]); 
+	}
+
+	lines = lines.concat([
+		'',
+  		'    Usage:',
+  		'        sms24x7-client send|help --email=email --password=password --phone=recipient_phone --message=message_text [--sender=alias]',
+  		'',
+  		'    Available commands:',
+      	'        send    Send text message to a recipient',
+      	'                    --email       Account email',
+      	'                    --password    Account password',
+      	'                    --phone       Recipient phone number',
+      	'                    --message     Message text',
+      	'                   --sender      Text string to show instead of server phone number',
+      	'',
+      	'        help    Show this help and exit',
+      	''
+	]);
+
+    console.log(lines.join('\n'));
+}
 
 
 
@@ -32,11 +64,24 @@ smsSender = new SmsSender({
 		password: cmdArgs.password
 	}
 }); 
-smsSender
-	.send(cmdArgs.phone, cmdArgs.message, cmdArgs.sender)
-	.then(function(response) {
-		console.log(response); 
-	})
-	.catch(function(error) {
-		console.log(error);
-	}); 
+
+
+switch (command) {
+	case 'send': 
+		smsSender
+			.send(cmdArgs.phone, cmdArgs.message, cmdArgs.sender)
+			.then(function(response) {
+				console.log(response); 
+			})
+			.catch(function(error) {
+				console.log(error);
+			}); 
+		break;
+
+	case 'help': 
+		printHelp(false);
+		break;
+
+	default:
+		printHelp(command);
+}
